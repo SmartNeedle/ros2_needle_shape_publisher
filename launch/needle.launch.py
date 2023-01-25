cb53d154b89a5d3868d4669f0a7c6cf75a6dd021
maribernardes <maribernardes@gmail.com>
2023-01-25 14:40:31 -0500

    Small fix in needle.launch.py Moved json needleParamFile to a subfolder inside needle_data

--- launch/needle.launch.py
+++ launch/needle.launch.py
@@ -3,8 +3,9 @@ import json
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 pkg_needle_shape_publisher = get_package_share_directory('needle_shape_publisher')
 
@@ -55,7 +56,7 @@ def generate_launch_description():
              PythonLaunchDescriptionSource(
                 os.path.join(pkg_needle_shape_publisher, 'sensorized_shapesensing_needle_decomposed.launch.py')),
                 launch_arguments = {
-                    'needleParamFile'   : PathJoinSubstitution( pkg_needle_shape_publisher, "needle_data", LaunchConfiguration( 'needleParamFile')),
+                    'needleParamFile'   : PathJoinSubstitution([pkg_needle_shape_publisher, 'needle_data', LaunchConfiguration('needleParamFile')]),
                     'numSignals'        : LaunchConfiguration('numSignals'),
                     'optimMaxIterations': LaunchConfiguration('optimMaxIterations'),
                     }.items()
